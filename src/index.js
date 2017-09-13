8611212c6b398a1e474c6072b448452539a08875
hildegw <hildegw@snafu.de>
2017-09-13 16:58:49 -0500

    installing dependencies, starting scaffolding

--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import { BrowserRouter } from "react-router-dom"
+import logger from 'redux-logger'
+import thunk from 'redux-thunk'
+import './index.css'
+import App from './components/App'
+import rootReducer from './reducers'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(logger, thunk),
+  )
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter><App /></BrowserRouter>
+  </Provider>,
+  document.getElementById('root')
+)
+
 registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,6 @@
+//import { ADD_RECIPE, REMOVE_FROM_CALENDAR } from '../actions'
+import { combineReducers } from 'redux'
+
+
+
+export default combineReducers({})
